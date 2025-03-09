6e3339a147b5e2889dc93ed59ffc6885162d6470
lmacadar <lmacadar@uci.edu>
2025-03-09 03:14:40 -0700

    Graph script
    graphing/graphData.py:
     - Finished the script to save graphs made from CSV's in graphing/data
     - Graphs are put in graphing/imageGraphs

--- graphing/graphData.py
+++ graphing/graphData.py
@@ -1,4 +1,28 @@
-import matplotlib as plt
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-if __name__ == "__main__":
-    pass
+titles = [
+    "n10",
+    "n100",
+    "n1000",
+    "n10000",
+    "s1"
+]
+
+if (__name__ == "__main__"):
+
+    for title in titles:
+        degrees = pd.read_csv(f"data/{title}_degrees.csv").to_numpy()
+
+        fig, ax = plt.subplots(1, 3, figsize=(12, 4))
+        
+        # Print degree graphs
+        colors      = ["red", "orange", "blue"]
+        graphTitles = ["In-degree distribution", "Out-degree distribution", "Undirected-degree distribution"]
+        for id in range(3):
+            ax[id].set_title(graphTitles[id])
+            ax[id].bar(range(degrees.shape[0]), degrees[:, id].T, color=colors[id])
+
+        plt.tight_layout()
+        plt.savefig(f"imageGraphs/{title}_graph.png")
